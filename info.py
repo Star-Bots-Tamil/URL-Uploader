350dff3a9aa11fd29fdf8ed6b5fe437e2f9c8dca
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-17 23:28:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,9 +19,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID', '11973721'))
-API_HASH = environ('API_HASH', '5264bf4663e9159565603522f58d3c18')
-BOT_TOKEN = environ('BOT_TOKEN', '6078234170:AAGX4CDjIN2owsTeYTFOzk47XACzFO1nZSA')
+API_ID = int(environ.get('API_ID', 11973721))
+API_HASH = environ.get('API_HASH', '5264bf4663e9159565603522f58d3c18')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6078234170:AAGX4CDjIN2owsTeYTFOzk47XACzFO1nZSA')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
